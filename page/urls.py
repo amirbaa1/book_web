557b813bed5e70cbf3e0ef7b7bf74dbf617bd751
amirbaa1 <amir.baahklag@yahoo.com>
2023-05-25 00:33:01 +0330

    fix home.html and fix upload update

--- page/urls.py
+++ page/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    #path('', HomePage.as_view(), name='home'),
+    path('', HomePage.as_view(), name='home'),
     path('about', AboutPage.as_view(), name='about'),
 ]
--- page/views.py
+++ page/views.py
@@ -3,10 +3,10 @@ from django.views.generic import TemplateView, ListView
 from book.models import Book
 
 
-# class HomePage(ListView):
-#     model = Book
-#     template_name = 'home.html'
-#     context_object_name = 'book_view'
+class HomePage(ListView):
+    model = Book
+    template_name = 'home.html'
+    context_object_name = 'book_view'
 
 
 class AboutPage(TemplateView):
